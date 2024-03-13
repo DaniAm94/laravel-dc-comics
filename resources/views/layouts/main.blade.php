95c09ac735d8ee1f4f17aa18d59bf71a049b9110
DaniAm94 <dani.amico94@gmail.com>
2024-03-13 17:39:26 +0100

    added fontawesome

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,12 +8,14 @@
     <title>{{ env('APP_NAME') }}</title>
     <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="images/ico">
     @vite('resources/js/app.js')
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+        integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
+        crossorigin='anonymous' />
     {{-- CDNS --}}
     @yield('cdns')
 </head>
 
-<body>
+<body class="body-bg">
 
     {{-- Header --}}
     @include('includes.header')
