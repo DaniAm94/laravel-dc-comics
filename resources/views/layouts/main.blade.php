bf00fe26c5f81dfcae27cad0fff6b3de943b1828
DaniAm94 <dani.amico94@gmail.com>
2024-03-13 18:27:47 +0100

    updated font family

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,15 @@
     <title>{{ env('APP_NAME') }}</title>
     <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="images/ico">
     @vite('resources/js/app.js')
+
+    {{-- Google Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
+    {{-- FontAwesome --}}
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
         integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
         crossorigin='anonymous' />
