3096604a2e2b61979c7f9ae75235cfa118c8fc6f
DaniAm94 <dani.amico94@gmail.com>
2024-03-13 16:43:45 +0100

    invoked comic controller into web file to handle the views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,59 +19,42 @@ Route::get('/', function () {
     return view('home', compact('title'));
 })->name('home');
 
-Route::get('/comics', function () {
-    $title = 'comics';
-    return view('comics/index', compact('title'));
-})->name('comics');
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 
-Route::get('/comics/{index}', function ($index) {
-    //Se l'index non rientra tra i possibili indici dell'array di comics allora visualizza la pagina 404 not found
-    if (!is_numeric($index) || $index < 0 || $index >= count(config('comics'))) abort(404);
-    $comics = config('comics');
-    return view('comics.show', ['comic' => $comics[$index]]);
-})->name('comic');
+Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comic.show');
 
 Route::get('/characters', function () {
-    $title = 'comics';
     return view('characters', compact('title'));
 })->name('characters');
 
 Route::get('/movies', function () {
-    $title = 'movies';
     return view('movies', compact('title'));
 })->name('movies');
 
 Route::get('/tv', function () {
-    $title = 'tv';
     return view('tv', compact('title'));
 })->name('tv');
 
 Route::get('/games', function () {
-    $title = 'games';
     return view('games', compact('title'));
 })->name('games');
 
 Route::get('/collectibles', function () {
-    $title = 'collectibles';
     return view('collectibles', compact('title'));
 })->name('collectibles');
 
 Route::get('/videos', function () {
-    $title = 'videos';
     return view('videos', compact('title'));
 })->name('videos');
 
 Route::get('/fans', function () {
-    $title = 'fans';
     return view('fans', compact('title'));
 })->name('fans');
 
 Route::get('/news', function () {
-    $title = 'news';
     return view('news', compact('title'));
 })->name('news');
 
 Route::get('/shop', function () {
-    $title = 'shop';
     return view('shop', compact('title'));
 })->name('shop');
