9854eea261870a061b99ad5ef58875973955d0e2
DaniAm94 <dani.amico94@gmail.com>
2024-03-14 17:17:37 +0100

    added fillable attributes to Comic model

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
+
+    protected $fillable = ['title', 'description', 'series', 'thumb', 'price', 'sale_date', 'artists', 'writers', 'type'];
     use HasFactory;
 }
