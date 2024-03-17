8b31d8b68eaa391c380a1dfb2f286e2a28b4f086
DaniAm94 <dani.amico94@gmail.com>
2024-03-17 17:38:13 +0100

    created a partial view for the alert

---
+++ resources/views/includes/alert.blade.php
@@ -0,0 +1,10 @@
+@if ($errors->any())
+    <div class="alert alert-danger mt-3">
+        <h4>Some fields are invalid!</h4>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
