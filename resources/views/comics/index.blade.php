27b32d3ca2f78da0173d8858965f33c29b0ed3b7
DaniAm94 <dani.amico94@gmail.com>
2024-03-18 16:13:05 +0100

    added a button to delete a comic on index and show views

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -14,10 +14,20 @@
                                 </figure>
                                 <span>{{ $comic->series }}</span>
                             </a>
-                            <a class="btn btn-on-card tooltip-parent" href="{{ route('comics.edit', $comic->id) }}">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                                <span class="my-tooltip">Update</span>
-                            </a>
+                            <div class="d-flex btn-on-card align-item-center">
+                                <a class="update-btn tooltip-parent d-flex align-items-center justify-content-center"
+                                    href="{{ route('comics.edit', $comic->id) }}">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                    <span class="my-tooltip">Update</span>
+                                </a>
+                                <form action="{{ route('comics.destroy', $comic->id) }}"
+                                    class=" tooltip-parent d-flex align-items-center" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn trash-btn"><i class="fa-solid fa-trash-can"></i></button>
+                                    <span class="my-tooltip">Delete</span>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 @endforeach
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -25,10 +25,20 @@
                 <div class="large-side">
                     <div class="d-flex justify-content-between ">
                         <h2><?= $comic['title'] ?></h2>
-                        <a class="btn tooltip-parent" href="{{ route('comics.edit', $comic->id) }}">
-                            <i class="fa-solid fa-pen-to-square"></i>
-                            <span class="my-tooltip">Update</span>
-                        </a>
+                        <div class="d-flex">
+                            <a class="update-btn tooltip-parent d-flex align-items-center justify-content-center"
+                                href="{{ route('comics.edit', $comic->id) }}">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                                <span class="my-tooltip">Update</span>
+                            </a>
+                            <form action="{{ route('comics.destroy', $comic->id) }}"
+                                class=" tooltip-parent d-flex align-items-center" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn trash-btn"><i class="fa-solid fa-trash-can"></i></button>
+                                <span class="my-tooltip">Delete</span>
+                            </form>
+                        </div>
                     </div>
                     <div class="green-bar">
                         <div class="green-info">
