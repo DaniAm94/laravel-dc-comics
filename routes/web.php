979a08ef6b646f05ab865a49b15422339145d08f
DaniAm94 <dani.amico94@gmail.com>
2024-03-18 15:30:09 +0100

    added a new route to delete a comic

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,12 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
-
-Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
-
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
-
+Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
-
-Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
-
 Route::put('/comics/{comic}', [ComicController::class, 'update'])->name('comics.update');
+Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
 
 Route::get('/characters', function () {
     return view('characters');
